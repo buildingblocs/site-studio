490c1796334e3700847fac9a787b923ae259db17
Aung Kaung Myat <69038584+icepcp@users.noreply.github.com>
2026-08-16 22:19:50 +0800

    Update about.ts

--- schemaTypes/about.ts
+++ schemaTypes/about.ts
@@ -1,62 +1,61 @@
-import { defineType, defineField } from "sanity";
-import { customBlock } from "../components/block";
+import {defineType, defineField} from 'sanity'
+import {customBlock} from '../components/block'
 
 export const aboutType = defineType({
-    name: "about",
-    title: "About Us",
-    type: "document",
-    fields: [
-        defineField({
-            name: "name",
-            title: "Page Name",
-            type: "string",
-            validation: (Rule) => Rule.required()
-        }),
-        defineField({
-            name: "latestEvent",
-            title: "Latest Event",
-            type: "reference",
-            to: [{ type: "event" }],
-            hidden: ({ document }) => !document?.name === "Archive"
-        }),
-        defineField({
-            name: "desc",
-            title: "Description",
-            type: "text",
-            validation: (Rule) => Rule.required()
-        }),
-        defineField({
-            name: "showList",
-            title: "Show List of Pages",
-            description: "Show list of pages in the directory rather than custom content",
-            type: "boolean"
-        }),
-        defineField({
-            title: "Slug",
-            name: "slug",
-            type: "slug",
-            options: {
-                source: "name",
-                maxLength: 200, // will be ignored if slugify is set
-                slugify: input => input
-                    .toLowerCase()
-                    .replace(/\s+/g, "-")
-                    .slice(0, 200)
-            },
-            validation: (Rule) => Rule.required()
-        }),
-        defineField({
-            name: "useImageHero",
-            title: "Hero with Background Image",
-            type: "boolean"
-        }),
-        defineField({
-            name: "imageHero",
-            title: "Images for Hero",
-            type: "array",
-            of: [{ type: "image" }],
-            hidden: ({ document }) => !document?.useImageHero
-        }),
-        customBlock
-    ]
-});
+  name: 'about',
+  title: 'About Us',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Page Name',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'latestEvent',
+      title: 'Latest Event',
+      type: 'reference',
+      to: [{type: 'event'}],
+      description: 'Used for the partners page to show the latest partners',
+      hidden: ({document}) => (document?.name ?? '') !== 'About Us',
+    }),
+    defineField({
+      name: 'desc',
+      title: 'Description',
+      type: 'text',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'showList',
+      title: 'Show List of Pages',
+      description: 'Show list of pages in the directory rather than custom content',
+      type: 'boolean',
+      hidden: ({document}) => (document?.name ?? '') !== 'About Us',
+    }),
+    defineField({
+      title: 'Slug',
+      name: 'slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 200, // will be ignored if slugify is set
+        slugify: (input) => input.toLowerCase().replace(/\s+/g, '-').slice(0, 200),
+      },
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'useImageHero',
+      title: 'Hero with Background Image',
+      type: 'boolean',
+    }),
+    defineField({
+      name: 'imageHero',
+      title: 'Images for Hero',
+      type: 'array',
+      of: [{type: 'image'}],
+      hidden: ({document}) => !document?.useImageHero,
+    }),
+    customBlock,
+  ],
+})
