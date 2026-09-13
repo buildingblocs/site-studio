68c14f12684d2adf24ec487ec187767f74547c5d
Aung Kaung Myat <69038584+icepcp@users.noreply.github.com>
2026-09-14 02:07:43 +0800

    add new dir

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -2,5 +2,6 @@ import { eventType } from "./event";
 import { aboutType } from "./about";
 import { siteInfoType } from "./siteInfo";
 import { partnerType } from "./partners";
+import { pageType } from "./page";
 
-export const schemaTypes = [eventType, aboutType, siteInfoType, partnerType];
+export const schemaTypes = [eventType, aboutType, siteInfoType, partnerType, pageType];
